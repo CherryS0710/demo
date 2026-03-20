0187b996e0693a7f498918403dad9c28f3d15afb
CherryS0710 <sharmacherry0710@gmail.com>
2026-03-20 15:57:58 +0530

    Feature changes

---
+++ demo/src/main/java/com/example/demo/model/StudentEntity.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+public class StudentEntity {
+    private String name;
+    private int rollNumber;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
